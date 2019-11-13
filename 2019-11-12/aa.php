76672b574f81db226b165db48fc7769ecfdff4a2
Paozi-ass <yxl19970407@163.com>
2019-11-13 19:39:24 +0800

    aaa

---
+++ 2019-11-12/aa.php
@@ -0,0 +1,24 @@
+<?php
+    $arr = $_FILES;
+    $info = $_REQUEST;
+    $ext = explode(".",$info['filename'][1]);
+    $fileName=$info['filename'];
+
+    $baseDir = "./".date("Y/m/d/",time());
+    if(!is_dir($baseDir)){
+        mkdir($baseDir,0,777);
+    }
+    $filePath = $baseDir.$fileName;
+
+    $tmpName = $arr['data']['tmp_name'];
+    $content =file_get_contents($tmpName);
+    file_put_contents($filePath,$content,FILE_APPEND);
+
+    $arrReturn = array(
+        "error"=>0,
+        "data"=>array(
+            'path'=>"/2019-11-12/".$filePath,
+),
+    );
+    echo json_encode($arrReturn);
+?>
---
+++ 2019-11-13/aa.php
@@ -0,0 +1,15 @@
+<?php
+//        $arr = $_FILES;
+//        var_dump($arr);
+    $arrInfo = $_FILES['Filedata'];
+//    var_dump($arrInfo);
+    $tmpName = $arrInfo['tmp_name'];
+    $realName = $arrInfo['name'];
+    $ext = explode(".",$realName)[1];
+    $baseName=md5(uniqid()).".$ext";
+    $baseDir = Date("Y/m/d/",time());
+    if(!is_dir($baseDir)){
+        mkdir($baseDir,0,777);
+    }
+    $filePath = $baseDir.$baseName;
+    move_uploaded_file($tmpName,$filePath);
